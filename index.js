7bec18823be7b8589ed94688844de882f0e827e2
Larissa Santana <lariihlorraine@gmail.com>
2023-06-15 19:02:21 -0300

    Adicionou botões para configurar dificuldade

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let randomNumber = 85; 
+let randomNumber = Math.floor(Math.random() * 100) + 1; 
 
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
@@ -7,8 +7,16 @@ const lowOrHigh = document.querySelector('.lowOrHigh');
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 
+const maxTtryText = document.querySelector('.maxTryText');
+const tent5 = document.querySelector('.tent5');
+const tent10 = document.querySelector('.tent10');
+const tent15 = document.querySelector('.tent15');
+
+
 let guessCount = 1;
 let resetGame;
+let maxTry = 10;
+maxTtryText.textContent = maxTry;
 
 function checkNumber() {
    
@@ -29,7 +37,7 @@ function checkNumber() {
             lowOrHigh.textContent = 'Número é baixo demais';
         }
 
-        if(guessCount == 10) {
+        if(guessCount == maxTry) {
             lastResult.textContent = 'Perdeu o jogo';
             guessSubmit.disabled = true;
             guessField.disabled = true;
@@ -44,4 +52,25 @@ function checkNumber() {
     guessField.focus();
 }
 
+function mudarSetup(value) {
+    maxTry = value;
+    maxTtryText.textContent = value;
+}
+
+function setup5() {
+    mudarSetup(5)
+}
+
+function setup10() {
+    mudarSetup(10)
+}
+
+function setup15() {
+    mudarSetup(15)
+}
+
+
 guessSubmit.addEventListener('click', checkNumber);
+tent5.addEventListener('click', setup5);
+tent10.addEventListener('click', setup10);
+tent15.addEventListener('click', setup15);
